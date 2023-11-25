dc50a9c113435dbf84e0467499e42053b0e7d162
adnan9899 <150807279+adnan9899@users.noreply.github.com>
2023-11-25 15:03:21 +0530

    Add files via upload

---
+++ Con.java
@@ -0,0 +1,15 @@
+package bank.management.system;
+import java.sql.*;
+public class Con {
+    Connection connection;
+    Statement statement;
+
+    public Con() {
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ bankSystem","root","Adnan@2003");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
